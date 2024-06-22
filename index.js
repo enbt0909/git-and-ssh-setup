4bb384c87aaa760881094f5e064180d13338f2ca
Eugen_Beirit <eugenbeirit22@gmail.com>
2024-06-23 00:33:58 +0200

    tabcontent

---
+++ index.js
@@ -0,0 +1,14 @@
+const agentSSH = (evt, agentName) => {
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(agentName).style.display = "block";
+  evt.currentTarget.className += " active";
+};
+
+document.getElementById("defaultOpen").click();
